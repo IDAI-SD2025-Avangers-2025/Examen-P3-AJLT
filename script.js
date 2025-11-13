68f78dbaf1ebcc65072bc68cee485f09e9a0692e
AngelL218 <al45850@un.edu.mx>
2025-11-13 12:33:49 -0600

    2

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function calcular() {
         if(especialidad == "clasica") {
     total = total + 80;}
 
-        if(especialidad == "bbq") {
+       else if(especialidad == "bbq") {
     total = total + 100;}
 
-        if(especialidad == "tocino") {
+       else if(especialidad == "tocino") {
     total = total + 120; }
 
-        if(especialidad == "doble") {
+       else if(especialidad == "doble") {
     total = total + 150; }
 
-        if(especialidad == "hawaiana") {
+       else if(especialidad == "hawaiana") {
     total = total + 180; }
 
     else{
@@ -44,73 +44,86 @@ function calcular() {
 
 
     if(formulario.queso.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.lechuga.checked == true){
-    tota += 5; }  
+    total += 5; }  
     
     if(formulario.jitomate.checked == true){
-    tota += 5; }    
+    total += 5; }    
 
     if(formulario.cebolla.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.jalapeño.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.huevo.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.pepino.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.champiñones.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.tocino.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.dobleQueso.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.piña.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.guacamole.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.bbq.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.chipotle.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.mayonesa.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.mostaza.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.ketchup.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.pepperoni.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.arugula.checked == true){
-    tota += 5; }
+    total += 5; }
 
     if(formulario.cebollaCaramelizada.checked == true){
-    tota += 5; }
+    total += 5; }
 
 
    var cantidad = document.getElementById("cantidad").value;
 
 
     if(formulario.domicilio.checked == true){
-    tota += 30; }
+    total += 30; }
 
 
-    formulario.total.value = total * cantidad;  
+    // formulario.total.value = 
 
+
+
+    if(pan == "brioche"){
+        if(formulario.jalapeño.checked == true){
+            if(formulario.champiñones.checked == true){
+                    alert("Has ganado una promoción");
+                    total = total - 20;
+        }
+    }
+ }
+
+     total = total * cantidad;  
+    document.getElementById('total').innerText = total
 }
